072654ec1070eee12f79a02111ac71c67ca91f3f
Jessy Hanifiah <jessyhanifiah@gmail.com>
2023-12-04 17:56:36 +0700

    chore(deps): add eslint-plugin-simple-import-sort

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import '@/styles/globals.css';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import '@/styles/globals.css';
+
 import { AuthProvider } from '@/components/Provider';
 
 const inter = Inter({ subsets: ['latin'] });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import { signIn } from 'next-auth/react';
+
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
